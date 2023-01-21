9a9ddb14f8583c7b939258a0af7d350dc59e89bc
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2023-01-21 01:24:30 +0100

    [UPDATE]

---
+++ src/main/java/com/ktd/ktdem/KtdemApplication.java
@@ -0,0 +1,34 @@
+package com.ktd.ktdem;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+@SpringBootApplication
+public class KtdemApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtdemApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		final Serde<String> stringSerde = Serdes.String();
+		final Serde<Long> longSerde = Serdes.Long();
+		final StreamsBuilder builder = new StreamsBuilder();
+
+		KStream<String, String> views = builder.stream(
+			"purchases",
+			Consumed.with(stringSerde, stringSerde)
+		);
+
+		
+	}
+
+}
---
+++ src/test/java/com/ktd/ktdem/KtdemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktd.ktdem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtdemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
